ff64a7a3ae8b36179f5256cdc7c910d955f89dbf
bdw617 <barry@defenseunicorns.com>
2023-06-05 13:58:06 -0400

    added additional test file

---
+++ capabilities/keycloak-authsvc/lib/authservice/secretConfig.test.ts
@@ -0,0 +1,49 @@
+import test from "ava";
+import { AuthserviceConfig, ChainInput, Match } from "./secretConfig";
+import { OIDCConfig } from "./oidcSectionConfig";
+
+test("AuthserviceConfig should handle input correctly", t => {
+  const chainInput: ChainInput = {
+    name: "test",
+    hostname: "test.hostname",
+    redirect_uri: "http://localhost:8080",
+    secret: "secret",
+  };
+
+  const filterChain = AuthserviceConfig.createSingleChain(chainInput);
+
+  // Check FilterChain properties
+  t.is(filterChain.name, chainInput.name);
+  t.truthy(filterChain.match);
+  t.truthy(filterChain.filters);
+
+  const match = filterChain.match as Match;
+  t.is(match.header, ":authority");
+  t.is(match.equality, chainInput.hostname);
+
+  const filter = filterChain.filters[0];
+  t.truthy(filter.oidc_override);
+
+  const oidcConfig = filter.oidc_override as OIDCConfig;
+  t.is(oidcConfig.callback_uri, chainInput.redirect_uri);
+  t.is(oidcConfig.client_id, chainInput.name);
+  t.is(oidcConfig.client_secret, chainInput.secret);
+  t.is(oidcConfig.cookie_name_prefix, chainInput.name);
+
+  const json = {
+    chains: [filterChain.toObject()],
+    listen_address: "localhost",
+    listen_port: 8080,
+    log_level: "debug",
+    threads: 1,
+  };
+
+  const authserviceConfig = new AuthserviceConfig(json);
+
+  // Check AuthserviceConfig properties
+  t.is(authserviceConfig.chains.length, 1);
+  t.is(authserviceConfig.listen_address, "localhost");
+  t.is(authserviceConfig.listen_port, 8080);
+  t.is(authserviceConfig.log_level, "debug");
+  t.is(authserviceConfig.threads, 1);
+});
