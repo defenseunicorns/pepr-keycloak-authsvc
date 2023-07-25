821543fae25385d58e87ece6c45746f9ada46e5b
Barry Waldbaum <barry@defenseunicorns.com>
2023-07-25 01:29:30 -0400

    Updated for better secret creation/deletion consistence  (#64)
    tested with 200 adds, and 200 deletes.

---
+++ capabilities/lib/authservice/secretBuilder.test.ts
@@ -0,0 +1,119 @@
+import anyTest, { TestFn } from "ava";
+
+import { AuthServiceSecretBuilder } from "./secretBuilder";
+import { V1Secret } from "@kubernetes/client-node";
+import { K8sAPI } from "../kubernetes-api";
+import { AuthserviceConfig } from "./secretConfig";
+
+const test = anyTest as TestFn<{
+  authServiceSecretBuilder: AuthServiceSecretBuilder;
+  testSecret: V1Secret;
+}>;
+
+test.beforeEach(t => {
+  const k8sApi = new K8sAPI();
+
+  // mock function to return secrets
+  k8sApi.getSecretsByLabelSelector = () => {
+    return Promise.resolve([
+      {
+        metadata: {
+          namespace: "default",
+          name: "foo",
+        },
+      },
+      {
+        metadata: {
+          namespace: "default",
+          name: "bar",
+        },
+      },
+    ]);
+  };
+
+  const secretBuilder = new AuthServiceSecretBuilder(k8sApi);
+
+  // mock authservice config
+  secretBuilder.getAuthServiceConfig = () => {
+    return Promise.resolve(new AuthserviceConfig({ chains: [] }));
+  };
+
+  t.context = {
+    authServiceSecretBuilder: secretBuilder,
+    testSecret: {
+      metadata: {
+        namespace: "default",
+        name: "baz",
+      },
+      data: {
+        id: "ZGV2XzAwZWI4OTA0LTViODgtNGM2OC1hZDY3LWNlYzBkMmUwN2FhNl9wb2RpbmZv",
+        name: "cG9kaW5mbw==",
+        domain: "YmlnYmFuZy5kZXY=",
+        realm: "YmFieS15b2Rh",
+        redirectUri: "YmlnYmFuZy5kZXY=",
+        clientSecret: "cG9kaW5mbw==",
+      },
+    },
+  };
+});
+
+test("AuthServiceSecretBuilder should handle adding a secret correctly", async t => {
+  const secrets = await t.context.authServiceSecretBuilder.buildSecretList(
+    t.context.testSecret,
+    false
+  );
+
+  t.is(secrets.length, 3);
+});
+
+test("AuthServiceSecretBuilder should handle deleting a secret correctly", async t => {
+  const deletedSecret: V1Secret = {
+    metadata: {
+      namespace: "default",
+      name: "foo",
+    },
+  };
+
+  const secrets = await t.context.authServiceSecretBuilder.buildSecretList(
+    deletedSecret,
+    true
+  );
+
+  t.is(secrets.length, 1);
+});
+
+test("AuthServiceSecretBuilder should handle sorting secrets correctly", async t => {
+  const secrets = await t.context.authServiceSecretBuilder.buildSecretList(
+    {
+      metadata: {
+        namespace: "otherns",
+        name: "foo",
+      },
+    },
+    false
+  );
+
+  t.is(secrets.length, 3);
+  t.is(secrets[0].metadata.name, "bar");
+  t.is(secrets[1].metadata.name, "foo");
+  t.is(secrets[2].metadata.name, "foo");
+  t.is(secrets[2].metadata.namespace, "otherns");
+});
+
+test("AuthServiceSecretBuilder should build an authservice config", async t => {
+  const authServiceConfig =
+    await t.context.authServiceSecretBuilder.buildAuthServiceConfig([
+      t.context.testSecret,
+    ]);
+
+  t.is(authServiceConfig.chains.length, 1);
+  t.is(authServiceConfig.chains[0].name, "podinfo");
+});
+
+test("AuthServiceSecretBuilder should leave a single chain if all are removed", async t => {
+  const authServiceConfig =
+    await t.context.authServiceSecretBuilder.buildAuthServiceConfig([]);
+
+  t.is(authServiceConfig.chains.length, 1);
+  t.is(authServiceConfig.chains[0].name, "placeholderName");
+});
--- capabilities/lib/authservice/secretBuilder.ts
+++ capabilities/lib/authservice/secretBuilder.ts
@@ -1,7 +1,14 @@
+import { Log } from "pepr";
 import { K8sAPI } from "../kubernetes-api";
 import { AuthserviceConfig } from "./secretConfig";
 import { V1Secret } from "@kubernetes/client-node";
 import { createHash } from "crypto";
+import { ascend, path, reject, sortWith } from "ramda";
+
+interface UpdateEvent {
+  secret: V1Secret;
+  isDelete: boolean;
+}
 
 export class AuthServiceSecretBuilder {
   k8sApi: K8sAPI;
@@ -24,28 +31,70 @@ export class AuthServiceSecretBuilder {
     return Buffer.from(secret.data[key], "base64").toString("utf-8");
   }
 
-  async getAuthServiceSecret(): Promise<AuthserviceConfig> {
-    const secretData = await this.k8sApi.getSecretValues(
-      this.authServiceSecretName,
-      this.authServiceNamespace,
-      [this.authServiceConfigFileName]
-    );
+  private sortSecrets(secrets: V1Secret[]): V1Secret[] {
+    return sortWith([
+      ascend(path(["metadata", "name"])),
+      ascend(path(["metadata", "namespace"])),
+    ])(secrets);
+  }
+
+  secretToAuthServiceConfig(secret: V1Secret): AuthserviceConfig {
+    const secretData = this.k8sApi.getSecretValues(secret, [
+      this.authServiceConfigFileName,
+    ]);
     return new AuthserviceConfig(
       JSON.parse(secretData[this.authServiceConfigFileName])
     );
   }
 
-  async buildAuthserviceSecret(labelSelector: string): Promise<string> {
-    const missionSecrets = await this.k8sApi.getSecretsByLabelSelector(
+  async update(e: UpdateEvent) {
+    await this.buildSecretList(e.secret, e.isDelete)
+      .then(this.buildAuthServiceConfig)
+      .then(this.updateAuthServiceSecret);
+  }
+
+  async buildSecretList(
+    updatedSecret: V1Secret,
+    isDelete: boolean,
+    labelSelector = "pepr.dev/keycloak=oidcconfig"
+  ): Promise<V1Secret[]> {
+    let missionSecrets = await this.k8sApi.getSecretsByLabelSelector(
       labelSelector
     );
 
-    if (missionSecrets.length == 0) {
-      return;
+    function isEqual(s: V1Secret) {
+      return (secret: V1Secret) =>
+        secret.metadata.name === s?.metadata?.name &&
+        secret.metadata.namespace === s?.metadata?.namespace;
     }
-    const authserviceConfig = await this.getAuthServiceSecret();
 
-    authserviceConfig.chains = missionSecrets.map(secret => {
+    // remove incoming secret if it exists
+    missionSecrets = reject(isEqual(updatedSecret), missionSecrets);
+
+    // if its an add/update op, add back to list
+    if (!isDelete) {
+      missionSecrets = [...missionSecrets, updatedSecret];
+    }
+
+    // sort by name, namespace
+    return this.sortSecrets(missionSecrets);
+  }
+
+  async getAuthServiceConfig(): Promise<AuthserviceConfig> {
+    const response = await this.k8sApi.k8sApi.readNamespacedSecret(
+      this.authServiceSecretName,
+      this.authServiceNamespace
+    );
+
+    return this.secretToAuthServiceConfig(response.body);
+  }
+
+  async buildAuthServiceConfig(
+    secrets: V1Secret[]
+  ): Promise<AuthserviceConfig> {
+    const authServiceConfig = await this.getAuthServiceConfig();
+
+    authServiceConfig.chains = secrets.map(secret => {
       const name = this.decodeBase64(secret, "name");
       const domain = this.decodeBase64(secret, "domain");
       const id = this.decodeBase64(secret, "id");
@@ -58,15 +107,46 @@ export class AuthServiceSecretBuilder {
       });
     });
 
+    // In the event that we've deleted the chain, create a placeholder to keep authservice from crashing
+    if (authServiceConfig.chains.length === 0) {
+      authServiceConfig.chains.push(
+        AuthserviceConfig.createSingleChain({
+          id: "placeholderId",
+          name: "placeholderName",
+          hostname: "localhost.localhost",
+          redirect_uri: "https://localhost.localhost",
+          secret: "placeholderSecret",
+        })
+      );
+    }
+
+    return authServiceConfig;
+  }
+
+  async updateAuthServiceSecret(authserviceConfig: AuthserviceConfig) {
     const config = JSON.stringify(authserviceConfig);
     const configHash = createHash("sha256").update(config).digest("hex");
 
-    await this.k8sApi.upsertSecret(
-      this.authServiceNamespace,
+    const didItWork = await this.k8sApi.patchSecret(
       this.authServiceSecretName,
-      { [this.authServiceConfigFileName]: config }
+      this.authServiceNamespace,
+      {
+        [this.authServiceConfigFileName]: config,
+      }
     );
 
-    return configHash;
+    if (didItWork) {
+      Log.info("Updated secret succesfully", "updateAuthServiceSecret");
+      await this.k8sApi.checksumDeployment(
+        "authservice",
+        "authservice",
+        configHash
+      );
+    } else {
+      Log.error(
+        "Patching AuthService Secret failed (out of sync)",
+        "updateAuthServiceSecret"
+      );
+    }
   }
 }
