2e335175513f1b5dcf625dbd8c801ac541367e3f
UnicornChance <chance@defenseunicorns.com>
2023-10-17 08:44:11 -0600

    fix: secret creation

--- e2e/e2e.test.ts
+++ e2e/e2e.test.ts
@@ -9,7 +9,7 @@ const execAsync = util.promisify(exec);
 test("E2E Test: Create New Client from Generic Secret", async t => {
 
   // Define the kubcetl command to create new secret to test integration
-  const createSecret = 'kubectl create secret generic client2 -n keycloak --from-literal=realm=baby-yoda --from-literal=id=podinfo --from-literal=name=podinfo --from-literal=domain=bigbang.dev'
+  const createSecret = 'kubectl create secret generic client2 -n keycloak --from-literal=keycloakBaseUrl=http://keycloak-http.keycloak.svc.cluster.local/auth --from-literal=realm=baby-yoda --from-literal=id=podinfo --from-literal=name=podinfo --from-literal=domain=bigbang.dev'
   const labelSecret = 'kubectl label secret client2 -n keycloak "pepr.dev/keycloak=createclient"';
 
   try{
