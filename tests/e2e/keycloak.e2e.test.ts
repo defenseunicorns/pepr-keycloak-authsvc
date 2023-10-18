70da72c71b73be2e5649cf7c68e3b1a138a95b2a
UnicornChance <chance@defenseunicorns.com>
2023-10-18 12:40:20 -0600

    wip: e2e tests

--- tests/e2e/keycloak.e2e.test.ts
+++ tests/e2e/keycloak.e2e.test.ts
@@ -8,36 +8,20 @@ const execAsync = util.promisify(exec);
 
 test.serial("E2E Test: Create New Client from Generic Secret", async t => {
   // Define the kubcetl command to create new secret to test integration
-  const createSecret =
-    "kubectl create secret generic client2 -n keycloak --from-literal=keycloakBaseUrl=http://keycloak-http.keycloak.svc.cluster.local/auth --from-literal=realm=master --from-literal=id=podinfo --from-literal=name=podinfo --from-literal=domain=bigbang.dev";
   const labelSecret =
-    'kubectl label secret client2 -n keycloak "pepr.dev/keycloak=createclient"';
+    'kubectl label secret client1 -n keycloak "pepr.dev/keycloak=createclient"';
 
   try {
-    //Execute createSecret
-    const { stdout, stderr } = await execAsync(createSecret);
+    const { stdout: labelout, stderr: labelerr } = await execAsync(labelSecret);
 
-    t.truthy(stdout, "Kubectl command to create new secret produced output");
+    t.truthy(labelout, "Kubectl command to label new secret produced output");
     t.falsy(
-      stderr,
-      "kubectl command to create new secret produced no stderr output",
+      labelerr,
+      "kubectl command to label new secret produced no stderr output",
     );
 
-    // if creating the new secret was successful change the label to
-    // trigger pepr-keycloak-authsvc changes
-    if (!stderr) {
-      const { stdout: labelout, stderr: labelerr } =
-        await execAsync(labelSecret);
-
-      t.truthy(labelout, "Kubectl command to label new secret produced output");
-      t.falsy(
-        labelerr,
-        "kubectl command to label new secret produced no stderr output",
-      );
-    }
-
     // Get the newly created secret that should have been created by pepr-keycloak-authsvc and keycloak
-    const getNewSecret = "kubectl get secret client2 -n keycloak";
+    const getNewSecret = "kubectl get secret client1 -n keycloak";
     const { stdout: newSecretOut, stderr: newSecretErr } =
       await execAsync(getNewSecret);
 
@@ -51,3 +35,76 @@ test.serial("E2E Test: Create New Client from Generic Secret", async t => {
   }
 });
 
+test.serial("E2E Test: Create a realm from generic secret", async t => {
+  const labelSecret =
+    "kubectl label secret realm1 -n keycloak  pepr.dev/keycloak=createrealm";
+
+  try {
+    const { stdout: labelout, stderr: labelerr } = await execAsync(labelSecret);
+
+    t.truthy(labelout, "Kubectl command to label new secret produced output");
+    t.falsy(
+      labelerr,
+      "kubectl command to label new secret produced no stderr output",
+    );
+
+    interface accessToken {
+      access_token: string;
+    }
+
+    const response = await fetch<accessToken>(
+      `http://localhost:8080/auth/realms/master/protocol/openid-connect/token`,
+      {
+        method: "POST",
+        body: `username=admin&password=password&grant_type=password&client_id=admin-cli`,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    );
+
+    await fetch(`http://localhost:8080/auth/admin/realms/`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${response.data.access_token}`,
+      },
+    })
+      .then(response => {
+        t.true(response.ok, "Request to get all realms should be successful");
+        return response.data as object[];
+      })
+      .then(data => {
+        t.true(data.length === 2);
+      });
+  } catch (e) {
+    t.fail("Failed to run kubectl command without errors: " + e.message);
+  }
+});
+
+test.serial("E2E Test: Delete a secret from keycloak", async t => {
+  // Define the kubcetl command to delete secret
+  const deleteSecret = "kubectl delete secret client1 -n keycloak";
+
+  try {
+    const { stdout: deleteOut, stderr: deleteErr } =
+      await execAsync(deleteSecret);
+
+    t.truthy(deleteOut, "Kubectl command to delete secret produced output");
+    t.falsy(
+      deleteErr,
+      "kubectl command to delete secret produced no stderr output",
+    );
+
+    // Attempt to get the newly deleted secret
+    try {
+      const getNewSecret = "kubectl get secret client1 -n keycloak";
+      await execAsync(getNewSecret);
+
+      t.fail("Secret should not exist anymore but it does.");
+    } catch (err) {
+      t.pass("Secret does not exist anymore as expected.");
+    }
+  } catch (e) {
+    t.fail("Failed to run kubectl command without errors: " + e.message);
+  }
+});
--- tests/e2e/keycloak.sh
+++ tests/e2e/keycloak.sh
@@ -4,7 +4,9 @@ helm repo add codecentric https://codecentric.github.io/helm-charts
 helm upgrade --install keycloak codecentric/keycloakx --namespace keycloak --create-namespace --values=values.yaml
 
 sleep 5 && kubectl wait --for=condition=ready pods -l app.kubernetes.io/name=keycloakx -n keycloak --timeout=5m
-kubectl apply -f test-secret.yaml
+kubectl apply -f client-secret.yaml
+kubectl apply -f realm-secret.yaml
+kubectl port-forward -n keycloak service/keycloak-http 8080:80 &
 
 
 
